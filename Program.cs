3e4244ee3c28e4d1c69f485d1670cecae7abc97a
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-12-07 14:56:40 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+if (args.Length == 0)
+{
+    var versionString = Assembly.GetEntryAssembly()?
+                                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                                .InformationalVersion
+                                .ToString();
+    Console.WriteLine($"Fon Version: {versionString}");
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine("Usage: fon <command> [options]");
+    return;
+}
+
