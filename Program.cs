76f39d22603773172c346ca816496ff4de70ef32
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-12-12 15:37:49 -0300

    carrega html

--- Program.cs
+++ Program.cs
@@ -1,20 +1,8 @@
-﻿
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-
-using (HttpClient client = new HttpClient())
-{
-    string username = "BacelarVitor";
-    string apiUrl = $"https://github.com/{username}";
-
-    client.DefaultRequestHeaders.Add("User-Agent", "MyApp"); // GitHub API requires a User-Agent header
-
-    HttpResponseMessage response = await client.GetAsync(apiUrl);
-    response.EnsureSuccessStatusCode();
-
-    string responseBody = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(responseBody);
-}
+﻿using HtmlAgilityPack;
 
 
+var web = new HtmlWeb();
+
+var user = "BacelarVitor";
+var doc = web.Load($"https://github.com/{user}");
+Console.WriteLine(doc.Text);
