ebc63953f5289b63f0959ac51db4650592ae51ff
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-12-13 15:42:43 -0300

    gets days of the graph

--- Program.cs
+++ Program.cs
@@ -5,4 +5,5 @@ var web = new HtmlWeb();
 
 var user = "BacelarVitor";
 var doc = web.Load($"https://github.com/{user}");
-Console.WriteLine(doc.Text);
+var fon = doc.DocumentNode.QuerySelectorAll("td.ContributionCalendar-day[data-date]");
+fon.ToList().ForEach(x => Console.WriteLine(x.OuterHtml));
